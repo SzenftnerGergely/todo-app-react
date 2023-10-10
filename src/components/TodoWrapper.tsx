5b096795b983abc96b0ceb071894fd5571b9cfea
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-10 15:28:21 +0200

    delete function implemented

--- src/components/TodoWrapper.tsx
+++ src/components/TodoWrapper.tsx
@@ -24,11 +24,20 @@ export default function TodoWrapper() {
       todo.id === id ? {...todo, completed: !todo.completed} : todo))
   }
 
+  const deleteTodo = (id: string) =>{
+    setTodos(todos.filter(todo => todo.id !== id))
+  } 
+
   return (
     <div className="TodoWrapper">
       <TodoForm addTodo={addTodo}/>
       {todos.map((todo, index) => (
-        <Todo task={todo} key={index} toggleComplete={toggleComplete}/>
+        <Todo 
+          task={todo} 
+          key={index} 
+          toggleComplete={toggleComplete}
+          deleteTodo={deleteTodo}
+        />
       ))}
     </div>
   )
