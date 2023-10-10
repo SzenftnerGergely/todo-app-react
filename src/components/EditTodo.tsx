66dbe6167e79c805ee3f8654b567622f66d17b24
SzenftnerGergely <gergely.szenftner@gmail.com>
2023-10-10 16:20:04 +0200

    edit function implemented

--- src/components/EditTodo.tsx
+++ src/components/EditTodo.tsx
@@ -1,8 +1,30 @@
+import { useState } from "react"
 
-export default function EditTodo() {
+export default function EditTodoForm({editTodo, task}) {
+  const [value, setValue] = useState(task.task)
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    editTodo(value, task.id)
+    setValue("")
+  } 
+  
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={handleSubmit} className="TodoForm">
+      <input 
+        type="text" 
+        value={value}
+        className="todo-input" 
+        placeholder="Update Task?" 
+        onChange={(event) => setValue(event.target.value)}
+      />
+      <button 
+        type="submit" 
+        className="bg-[#8758ff] text-white p-[0.55rem] ml-2 
+        border-none cursor-pointer active:scale-95"
+      >Update Task
+      </button>
+    </form>
   )
 }
